5cdd0a5baac7946440d140c349dca3af55dd5b56
rcooke-warwick <ryan@balena.io>
2023-02-22 11:09:20 +0000

    remove testbot sdk
    Change-type: patch
    Signed-off-by: Ryan Cooke <ryan@balena.io>

--- lib/features/sd-mux/index.ts
+++ lib/features/sd-mux/index.ts
@@ -1,11 +1,9 @@
 import { AutoKitSdMux } from "./implementations/autokit-sd-mux";
-import { TestbotSdMux } from "./implementations/testbot-sd-mux";
 import { LinuxAut } from "./implementations/linux-gmbh";
 import { DummySdMux } from "./implementations/dummy-sd-mux";
 
 const sdMuxImplementations: {[key: string]: Type<SdMux> } = {
 	autokitSdMux: AutoKitSdMux,
-	testbotSdMux: TestbotSdMux,
 	linuxAut: LinuxAut,
 	dummySdMux: DummySdMux,
 };
