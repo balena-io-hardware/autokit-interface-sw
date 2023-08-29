f848aa13648f20917f2ad9747019a8b96ce9864b
rcooke-warwick <ryan@balena.io>
2023-08-29 15:45:40 +0100

    add sdwire to implementation list
    Change-type: patch
    Signed-off-by: Ryan Cooke <ryan@balena.io>

--- lib/features/sd-mux/index.ts
+++ lib/features/sd-mux/index.ts
@@ -1,9 +1,11 @@
 import { AutoKitSdMux } from "./implementations/autokit-sd-mux";
 import { LinuxAut } from "./implementations/linux-gmbh";
 import { DummySdMux } from "./implementations/dummy-sd-mux";
+import { SdWire } from "./implementations/sdwire";
 
 const sdMuxImplementations: {[key: string]: Type<SdMux> } = {
 	autokitSdMux: AutoKitSdMux,
+	sdWire: SdWire,
 	linuxAut: LinuxAut,
 	dummySdMux: DummySdMux,
 };
