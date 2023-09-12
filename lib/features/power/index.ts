d58c070ca03fcacabe509ad1d69d5d3593d3b517
rcooke-warwick <ryan@balena.io>
2023-09-12 11:39:00 +0100

    add crelay implementation
    Change-type: patch
    Signed-off-by: Ryan Cooke <ryan@balena.io>

--- lib/features/power/index.ts
+++ lib/features/power/index.ts
@@ -1,9 +1,11 @@
 import { AutokitRelay } from "./implementations/autokit-relay";
 import { DummyPower } from "./implementations/dummy-power";
+import { Crelay } from "./implementations/crelay";
 
 const powerImplementations: {[key: string]: Type<Power> } = {
 	autokitRelay: AutokitRelay,
 	dummyPower: DummyPower,
+	crelay: Crelay,
 };
 
 export { powerImplementations }
