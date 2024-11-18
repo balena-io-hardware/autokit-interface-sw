a3b62918d9273346da8525ef0e64303ef789c339
Ryan Cooke <ryan@balena.io>
2024-11-18 13:12:34 +0000

    power: add override for tx2 device type
    This adds a subclass for use with the tx2 device type derived from autokitRelay() - it modifies the on and off functions to use the tx2 button press sequence instead
    
    Change-type: patch
    Signed-off-by: Ryan Cooke <ryan@balena.io>

--- lib/features/power/index.ts
+++ lib/features/power/index.ts
@@ -1,11 +1,13 @@
 import { AutokitRelay } from "./implementations/autokit-relay";
 import { DummyPower } from "./implementations/dummy-power";
 import { Crelay } from "./implementations/crelay";
+import { JetsonTx2Power } from "./implementations/autokit-relay/jetson-tx2";
 
 const powerImplementations: {[key: string]: Type<Power> } = {
 	autokitRelay: AutokitRelay,
 	dummyPower: DummyPower,
 	crelay: Crelay,
+	jetsonTx2: JetsonTx2Power
 };
 
 export { powerImplementations }
